213db829e96ed958eddff6bb0c01a3c02e86053a
Devender Kumar <lucdev93@gmail.com>
2017-12-10 12:11:57 +0530

    Delete data from table

---
+++ Delete.php
@@ -0,0 +1,25 @@
+ <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to delete a record
+$sql = "DELETE FROM Tablename WHERE id= id";
+// you need to  replace the id with your id and table name //
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+?> 
